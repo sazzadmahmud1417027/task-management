1865f5df8a0a9b1d34e35fb3d47f4f2882958892
sazzadmahmud1417027 <tusher@appnap.io>
2020-01-29 17:21:35 +0600

    all done

--- app.js
+++ app.js
@@ -8,21 +8,59 @@ const taskInput=document.querySelector("#task");
 loadAllEventListeners();
 function loadAllEventListeners(){
     form.addEventListener('submit',addTask);
+    //remove single task
+    tasklist.addEventListener('click',removeTask);
+    //clear all task
+    clearBtn.addEventListener('click',clearTask);
+    //filter task
+    filter.addEventListener('keyup',filterTasks);
 }
 function addTask(e){
     if(taskInput.value==''){
         alert('Add a task');
     }
+    else{
+        const li = document.createElement("li");
+        //adding a class
+        li.className='collection-item';
+        //createclassname and append to li
+        li.appendChild(document.createTextNode(taskInput.value));
+        const link = document.createElement('a');
+        link.className = 'delete-item secondary-content';
+        link.innerHTML = '<i class="fa fa-remove"></i>'
+        li.appendChild(link);
+        tasklist.appendChild(li);
+        e.preventDefault();
+    }
     //create li element
-    const li = document.createElement("li");
-    //adding a class
-    li.className='collection-item';
-    //createclassname and append to li
-    li.appendChild(document.createTextNode(taskInput.value));
-    const link = document.createElement('a');
-    link.className = 'delete-item secondary-content';
-    link.innerHTML = '<i class="fa fa-remove"></i>'
-    li.appendChild(link);
-    console.log(li);
-    e.preventDefault();
+
+}
+function removeTask(e){
+    if(e.target.parentElement.classList.contains('delete-item')){
+        if(confirm("Are you sure? You want to remove it")){
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+function clearTask(e){
+    if(tasklist.children.length!=0){
+        while(tasklist.firstChild){
+            tasklist.removeChild(tasklist.firstChild);
+        }
+    }
+   else{
+       alert("Nothing to remove");
+    }
+}
+function filterTasks(e){
+    const text=e.target.value.toLowerCase();
+    const taski = document.querySelectorAll('.collection-item').forEach(function(task){
+        const item=task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) !=-1){
+            task.style.display = 'block';
+        }
+        else{
+            task.style.display = 'none';
+        }
+    });
 }
